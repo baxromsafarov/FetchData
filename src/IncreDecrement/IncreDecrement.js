b5f2219c1aec635c02616bbd2dec4f1073653c08
Jdu214970 <214970b@jdu.uz>
2023-06-22 17:17:56 +0500

    count changer

---
+++ src/IncreDecrement/IncreDecrement.js
@@ -0,0 +1,25 @@
+import  {useState} from "react";
+import "./IncreDecrement.css";
+
+const IncreDecrement = () =>{
+    const [increDecrement, setIncreDecrement] = useState(0);
+
+    const incrementHandler = () =>{
+        setIncreDecrement(increDecrement + 1);
+    }
+
+    const decrementHandler = () =>{
+        setIncreDecrement(increDecrement - 1);
+    }
+
+    return (
+        <div className="increDecrement">
+            <h1>Count Changer</h1>
+            <h2>{increDecrement}</h2>
+            <button onClick={incrementHandler}>Push to +</button>
+            <button onClick={decrementHandler}>Push to -</button>
+        </div>
+    );
+}
+
+export default IncreDecrement;
